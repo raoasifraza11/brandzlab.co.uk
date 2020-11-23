ebef24031a9ecc261d4ea6c34d5a2638858398ea
raoasifraza11 <raoasifraza1@hotmail.com>
2020-11-23 16:55:31 +0500

    Update db

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '123',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'portfolio',
